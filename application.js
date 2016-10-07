5c914747d38c168e582431496df0c8bbecb09829
Shane Perreault <shaneprrlt@gmail.com>
2016-10-07 03:21:21 -0500

    mobile compatible

--- application.js
+++ application.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
+  var subtitles = [
+    "Looking for a better solution to Facebook Monetization? All you had to do was look to the moon.",
+    "Use ApolloKit to easily find, schedule, and post content on your Facebook Pages."
+  ],
+  subtitle = subtitles[Math.floor(Math.random() * subtitles.length)];
+  $('.subtitle').html(subtitle);
+
   $("#ctaSignup").on('click', function(evt) {
     evt.preventDefault();
 
     swal({
       title: "Signup",
-      text: 'We\'re still building, enter your email to be notifed when we launch.',
+      text: 'We\'re still building, enter your email address or phone number to be notifed when we launch.',
       type: 'input',
       showCancelButton: true,
       closeOnConfirm: false,
+      inputPlaceholder: 'Email Address or SMS Number',
     }, function(emailAddress){
 
       if (emailAddress && emailAddress.length > 0) {
