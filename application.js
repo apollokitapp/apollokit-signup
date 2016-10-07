0c733efe4b6f783fd46ba39fce84eb868a39ae89
Shane Perreault <shaneprrlt@gmail.com>
2016-10-06 20:57:27 -0500

    almost finished

---
+++ application.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#ctaSignup").on('click', function(evt) {
+    evt.preventDefault();
+
+    swal({
+      title: "Signup",
+      text: 'We\'re still building, enter your email to be notifed when we launch.',
+      type: 'input',
+      showCancelButton: true,
+      closeOnConfirm: false,
+    }, function(emailAddress){
+
+      $.ajax({
+        url: 'https://formspree.io/apollokitapp_signup@gmail.com',
+        method: 'post',
+        data: { message: emailAddress },
+        dataType: 'json',
+        success: function() {
+          swal("Thanks! Stay Tuned. :)");
+        }
+      });
+
+    });
+  });
+});
